9fe557905c9525de74d29faae329b326e5b42e46
KajalMukherje <119003258+KajalMukherje@users.noreply.github.com>
2023-08-11 15:39:07 +0000

    DataTypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,16 @@
+"use strict" // treat all JS code as the newer version
+//alert(3+3) // not working because we are using node not browser
+//alert(3+
+ //   3) // code readability should be high
+// Data type
+let name = "Kajal"                // string type
+let age = 18                     // number type
+let loggedIn = true;            // boolean type
+let temperature = null         // null type
+let state;                    // undefined type
+                             //symbol type => mostly use for unique
+                            //bigint 
+
+                           //object type
+
+console.log(typeof "kajal");
