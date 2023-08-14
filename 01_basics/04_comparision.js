7858ad40eeee2a81aef5aadc42435af4ab9b8ec7
KajalMukherje <119003258+KajalMukherje@users.noreply.github.com>
2023-08-14 12:13:05 +0000

    compare

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+console.log("2" > 2);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+console.log("33ab" > 33);
+
+console.log("2" == 2);
+
+//Strict equality comparison
+console.log("2" === 2);
